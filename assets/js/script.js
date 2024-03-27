0131d2d800e37b202fd3d10d3457c48b0b37efd1
Eider Agudelo <eider841agudelo@hotmail.com>
2024-03-27 19:00:15 -0300

    Validar Formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+/**
+ * Validación de datos, 
+ * busca palabra Perro o Gato
+ */
 var buscar = document.getElementById("buscar")
 
 //Evento click, funcion validar
-buscar.addEventListener('click', validar)
+buscar.addEventListener('click', validarPerroGato)
 
-function validar() {
+function validarPerroGato() {
     var animal = document.querySelector(".animal").value
     var patron1 = /gato/i
     var patron2 = /perro/i
@@ -12,5 +16,24 @@ function validar() {
         alert("Palabra ingresada permitida");
     } else {
         alert("La palabra ingresada no es permitida");
+    }
+}
+
+/**
+ * Validación de datos,
+ * Validas las palabras y no los numeros
+ */
+
+var enviar = document.getElementById("enviar")
+enviar.addEventListener('click', validacion)
+
+function validacion() {
+    var lugar = document.querySelector(".lugar").value
+    var permitido = /[a-zA-Z]/gim
+
+    if (lugar.match(permitido)) {
+        alert("Texto Permitido")
+    } else {
+        alert("Texto no permitido")
     }
 }
